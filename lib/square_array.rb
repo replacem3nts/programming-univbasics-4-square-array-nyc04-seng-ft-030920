d6acb5c0afb113493fe851038679b760ae844dce
Donny Landis <donny.landis@gmail.com>
2020-02-09 22:42:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   counter = 0
-
+  new_array = []
+  
   while array[counter] do
-   square_array << (array[counter] ** 2)
+   new_array << (array[counter] ** 2)
    counter += 1
   end
-  return square_array
+  return new_array
 end
